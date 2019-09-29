876aff15f7ef26b72782e6018bf8f637d0951fb9
andresgr20 <andresgr2094@gmail.com>
2019-09-28 20:07:14 -0400

    Added req _code and exit codes when terminate is called

--- A1/client.py
+++ A1/client.py
@@ -1,36 +1,37 @@
-# first of all import the socket library 
-import socket                
-  
-# next create a socket object 
-s = socket.socket()
-print('Socket successfully created')
-  
-# reserve a port on your computer in our 
-# case it is 12345 but it can be anything 
-port = 12345                
-  
-# Next bind to the port 
-# we have not typed any ip in the ip field 
-# instead we have inputted an empty string 
-# this makes the server listen to requests  
-# coming from other computers on the network 
-s.bind(('', port))         
-print ("socket binded to %s" %(port) )
-  
-# put the socket into listening mode 
-s.listen(5)      
-print ("socket is listening" )           
-  
-# a forever loop until we interrupt it or  
-# an error occurs 
-while True: 
-  
-   # Establish connection with client. 
-   c, addr = s.accept()      
-   print('Got connection from', addr )
-  
-   # send a thank you message to the client.  
-   c.send('Thank you for connecting') 
-  
-   # Close the connection with the client 
-   c.close() 
+#!/usr/bin/env python
+
+import socket,sys
+
+# if len(sys.argv) != 5:
+#     print('Missing arguments! Script, servers address, port, req_code, message')
+#     exit()
+# serverName = str(sys.argv[1])
+# port = int(sys.argv[2])
+# code = int(sys.argv[3])
+# msg = str(sys.argv[4])
+serverName = 'localhost'
+serverPort = 12000
+code = 30
+# client to get the negotation port for the server UTP
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+clientSocket.send(code.encode())
+response = clientSocket.recv(1024).decode()
+
+#check if the validation from the server
+if(response == '0'):
+    print('Invalid req_code')
+    clientSocket.close()
+    exit()
+
+# change the port to the one given by the server
+port = int(response)
+clientSocket.close()
+
+
+
+sentence = raw_input('Input lowercase sentence:')
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print('From Server: ', modifiedSentence.decode())
+clientSocket.close()
--- A1/server.py
+++ A1/server.py
@@ -1,36 +1,55 @@
-# first of all import the socket library 
-import socket                
-  
-# next create a socket object 
-s = socket.socket()
-print('Socket successfully created')
-  
-# reserve a port on your computer in our 
-# case it is 12345 but it can be anything 
-port = 12345                
-  
-# Next bind to the port 
-# we have not typed any ip in the ip field 
-# instead we have inputted an empty string 
-# this makes the server listen to requests  
-# coming from other computers on the network 
-s.bind(('', port))         
-print ("socket binded to %s" %(port) )
-  
-# put the socket into listening mode 
-s.listen(5)      
-print ("socket is listening" )           
-  
-# a forever loop until we interrupt it or  
-# an error occurs 
-while True: 
-  
-   # Establish connection with client. 
-   c, addr = s.accept()      
-   print('Got connection from', addr )
-  
-   # send a thank you message to the client.  
-   c.send('Thank you for connecting') 
-  
-   # Close the connection with the client 
-   c.close() 
+#!/usr/bin/env python
+
+import socket,sys
+
+if len(sys.argv) != 2:
+    print('Missing the req_code')
+    exit()
+code = int(sys.argv[1])
+
+# port for the server
+serverPort = 12000
+negPort = 65534
+
+#Validation UTP protocol connection
+valSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+valSocket.bind(('localhost', serverPort))
+# listen sets the number of active clients for the server
+# set to 10 for testing
+valSocket.listen(10)
+print('The server is waiting for the req_code')
+connectionSocket, addr = valSocket.accept()
+msg = connectionSocket.recv(1024).decode()
+# Terminates the connection if the user enters TERMINATE
+if( msg != code):
+    connectionSocket.send('0')
+    print('Invalid req_code')
+else:
+    connectionSocket.send(negPort)
+    negPort=+1
+    connectionSocket.close()
+
+    # sets the socket to udp protocol
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DTREAM)
+    serverSocket.bind(('localhost', serverPort))
+    # listen sets the number of active clients for the server
+    # set to 10 for testing
+    serverSocket.listen(10)
+    print('The server is ready to receive messages')
+    msgs = []
+    clients = []
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        msg = connectionSocket.recv(1024).decode()
+        if(msg == "GET"):
+            if not msgs: #If there are no messages stored
+                connectionSocket.send("NO MSG")
+        # Terminates the connection if the user enters TERMINATE
+        if( msg == 'TERMINATE'):
+            connectionSocket.send('Shutting down server')
+            print('Server shut down')
+            connectionSocket.close()
+            exit()
+        msgs.append(msg)
+        connectionSocket.send('Client 1 Message:'+ msg.encode())
+        connectionSocket.close()
